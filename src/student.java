5684c6c893b3bcb2923ae182913ea2fef0b6a01d
philipmayowa01 <philipmayowa03@gmail.com>
2016-12-06 00:11:56 +0100

    assignment

---
+++ src/student.java
@@ -0,0 +1,10 @@
+/**
+ * Created by g on 12/6/2016.
+ */
+public class student {
+    public static void main(String[]args){
+    String name;
+     name  = "oladokun philip mayowa";
+        System.out.println(name);
+ }
+}
